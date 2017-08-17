ca1446f57e6c0d1e69dbba0388e78dacdb0e70ed
Daria Ganchukova <umi194@gmail.com>
2017-08-17 21:35:48 +0300

    added slider

--- js/js.js
+++ js/js.js
@@ -2,3 +2,47 @@
 	$('[data-toggle-menu]').removeClass("active");
 	$(this).addClass("active");
 });*/
+
+var buttons = document.querySelectorAll('.js-btn');
+var slide = document.querySelector('.js-slide');
+
+function clear(arr) {
+	arr.forEach(function(element) {
+		if (element.classList.contains('active')) {
+			element.classList.remove('active');
+		}
+	});
+}
+
+var i = 0;
+var start = setInterval(function() {
+	i++;
+	if (i < buttons.length) {
+		clear(buttons);
+		buttons[i].classList.add('active');
+		var w = slide.offsetWidth;
+		console.log('width = ', w);
+		slide.style.marginLeft = - w * i + 'px';
+	} else {
+		i = 0;
+		clear(buttons);
+		buttons[i].classList.add('active');
+		var w = slide.offsetWidth;
+		console.log('width = ', w);
+		slide.style.marginLeft = - w * i + 'px';
+	}
+}, 3000)
+
+buttons.forEach(function(element, index) {
+	element.addEventListener('click', function() {
+		i = index;
+		console.log('index = ', i);
+		clear(buttons);
+		this.classList.add('active');
+		var w = slide.offsetWidth;
+		console.log('width = ', w);
+		slide.style.marginLeft = - w * i + 'px';
+		clearInterval(start);
+	});
+});
+
